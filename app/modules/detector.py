e5db5c569fcfc68dfbc8b1648ff5f71d19df63df
Rafael Funchal <funchal.rafael@gmail.com>
2025-08-16 20:54:34 -0300

    model task parameter name fix

--- app/modules/detector.py
+++ app/modules/detector.py
@@ -3,7 +3,7 @@ import logging
 
 def load_yolo(model_path):
     try:
-        model = YOLO(model_path, tast="detect", verbose=False)
+        model = YOLO(model_path, task="detect", verbose=False)
         logging.info(f"Modelo YOLO carregado de {model_path}")
         return model
     except Exception as e:
